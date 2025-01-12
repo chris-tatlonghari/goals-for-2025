49999e229e08543206fcde2fb75d965f7811ee01
chris-tatlonghari <ChrisTat98@gmail.com>
2025-01-12 10:04:10 -0500

    Update passkeys #2

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,14 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 # Temporary until I switch to paid instance of Render
-User.find_or_create_by!(first_name: 'Chris', passkey: '95194')
-User.find_or_create_by!(first_name: 'Jake', passkey: '56072')
-User.find_or_create_by!(first_name: 'Caleb', passkey: '58785')
-User.find_or_create_by!(first_name: 'Jimmy', passkey: '88172')
-User.find_or_create_by!(first_name: 'Brycen', passkey: '45888')
+User.find_or_create_by!(first_name: 'Chris')
+User.find_or_create_by!(first_name: 'Jake')
+User.find_or_create_by!(first_name: 'Caleb')
+User.find_or_create_by!(first_name: 'Jimmy')
+User.find_or_create_by!(first_name: 'Brycen')
+
+User.find_by(first_name: 'Chris').update!(passkey: '95194')
+User.find_by(first_name: 'Jake').update!(passkey: '56072')
+User.find_by(first_name: 'Caleb').update!(passkey: '58785')
+User.find_by(first_name: 'Jimmy').update!(passkey: '88172')
+User.find_by(first_name: 'Brycen').update!(passkey: '45888')
