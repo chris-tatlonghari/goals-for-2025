41d6cc2d61799289dd7b8cd7e6b3ebcecb3f3f39
chris-tatlonghari <ChrisTat98@gmail.com>
2025-01-19 17:44:44 -0500

    Add daily progress updater

--- app/controllers/Goals/tracking_controller.rb
+++ app/controllers/Goals/tracking_controller.rb
@@ -6,6 +6,8 @@ module Goals
       @daily_goals = Array.new(5) do
         (1..365).map { |key| [key, rand(0..4) * rand(0..1)] }.to_h
       end
+
+      @goal_names = ['Exercise', 'Meditation', 'Reading', 'Coding', 'Writing']
     end
   end
 end
